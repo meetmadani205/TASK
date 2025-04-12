430cd9042a0144566332cf1321749e514f5bafc2
Meet Madani <142378084+meetmadani205@users.noreply.github.com>
2025-04-12 12:02:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+var typed = new Typed('#typed', {
+    strings: ['Web devlopment.','Java programming.' ,'DSA.','Problem solving.','Mobile App development'],
+    typeSpeed: 90,
+    backspeed : 90,
+    backdelay : 800,
+    loop : true,
+  });
